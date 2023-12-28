56cec50d0fd7e2eb02518ec4b84f9f915a166cc8
Liang_You <felixchen00000@gamil.com>
2023-12-28 19:20:27 +0800

    12/28

--- scripts/distributed/4machine/runMachine4.sh
+++ scripts/distributed/4machine/runMachine4.sh
@@ -3,7 +3,7 @@
 sshpass -p nsd ssh -X nsdlab@140.119.164.32 -p 9013 << MACHINE1
 echo 'Access into machine 1 (port:9010)'
 
-cd /home/nsdlab/liang_you_git_repo/Distributed_Banking_Project/scripts/distributed
+cd /home/nsdlab/liang_you_git_repo/Distributed_Banking_Project/scripts/distributed/4machine
 ./run2.sh $1 $2 $3 $4 $5 $6 $7 $8
 
 echo 'Exit machine 4 (port:9013)'
